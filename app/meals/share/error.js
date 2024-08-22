f9e443f934fd4126aa78c87fe1811eaa2f764e44
webDevT <karinadesyatnik27@gmail.com>
2024-08-22 09:13:25 +0300

    food app

---
+++ app/meals/share/error.js
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>An arror occurred!</h1>
+            <p>Failde to create meal</p>
+        </main>
+    )
+
+}
